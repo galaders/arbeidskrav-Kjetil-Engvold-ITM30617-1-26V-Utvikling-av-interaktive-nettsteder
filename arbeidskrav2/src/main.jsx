d399872c23ef9d0f4b52fba0d45e3530f809e2b5
galaders <kjetil.engvold@outlook.com>
2026-02-10 14:53:33 +0100

    fix av link

--- arbeidskrav2/src/main.jsx
+++ arbeidskrav2/src/main.jsx
@@ -1,7 +1,15 @@
 // link til ai chaten som er brukt for arbeidskravet
-// https://copilot.microsoft.com/shares/FU51dYRx8umXQE9c6Q6Tf
+// https://copilot.microsoft.com/conversations/join/kKmEZ8gFMq4NmDsAEyPF7
+// ser ut som du må joine chatten for å se infon det står...
+
+// Join 'React Handleliste-App Krav og Implementering'
+// You're invited to join Galned Engvold in a group chat with Copilot.
+// Copilot uses AI and can make mistakes. None of your personal 
+// memory with Copilot will be shared in the group.
+
 //ble en del siden ikke helt satt foten in på import og alt slik og deling av koder på 
 //forskelige sider må bli bedre på dette og øve på det
+
 
 
 
