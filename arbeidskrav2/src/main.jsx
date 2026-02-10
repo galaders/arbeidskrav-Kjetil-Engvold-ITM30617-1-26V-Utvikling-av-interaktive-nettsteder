8ba6d3a1cfdc76f026c35539b137f209639fd084
galaders <kjetil.engvold@outlook.com>
2026-02-10 14:42:14 +0100

    fix av links

--- arbeidskrav2/src/main.jsx
+++ arbeidskrav2/src/main.jsx
@@ -1,3 +1,10 @@
+// link til ai chaten som er brukt for arbeidskravet
+// https://copilot.microsoft.com/shares/FU51dYRx8umXQE9c6Q6Tf
+//ble en del siden ikke helt satt foten in på import og alt slik og deling av koder på 
+//forskelige sider må bli bedre på dette og øve på det
+
+
+
 import { StrictMode } from 'react' // Importer StrictMode fra React
 import { createRoot } from 'react-dom/client' // Importer createRoot fra react-dom/client
 import './index.css' // Importer CSS for styling
